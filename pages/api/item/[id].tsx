8a3bf912b0ec0a9a053c49cf006072bbcf7427c4
Ibrahim Ferreira <ibrahim.ferreira@gmail.com>
2021-12-01 21:28:42 +0100

    correct id on item

--- pages/api/item/[id].tsx
+++ pages/api/item/[id].tsx
@@ -85,6 +85,39 @@ export default async (
     res.end();
 
     //
+  } else if (req.method === 'PATCH') {
+    const session = await getSession({ req });
+
+    // TODO:
+    // if (!session) {
+    //   res.status(400).json({ error: ` Please login first!` });
+    //   return;
+    // }
+
+    const { db } = await connect();
+
+    const { id } = req.query;
+
+    if (!id) {
+      res.status(400).json({ error: ` Missing Id on request body` });
+      res.end();
+      return;
+    }
+
+    const response = await db.collection('items').deleteOne({
+      _id: new ObjectId(id),
+    });
+
+    if (!response) {
+      res.status(400).json({ error: `The item with ID=${id} was not found` });
+      res.end();
+      return;
+    }
+
+    res.status(200).json({ message: `Item deleted successfully` });
+    res.end();
+
+    //
   } else {
     res.status(400).json({ error: ` Wrong request method!` });
     res.end();
